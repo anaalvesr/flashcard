3ed1d2c5c742897b5d3b3f50c12038d8348166a9
anaalvesr <00001122138556sp@al.educacao.sp.gov.br>
2024-11-23 16:24:12 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do Sistema Solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a obra "Mona Lisa"?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a queba do Muro de Berlim?',
+    '1989'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água',
+    'H₂O.'
+)
